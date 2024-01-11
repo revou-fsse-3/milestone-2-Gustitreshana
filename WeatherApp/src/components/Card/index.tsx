e4ff44ec667c6b03826f546f761d821970615974
Gustitreshana <gustith2105@gmail.com>
2024-01-12 02:21:43 +0700

    Card for 3 hr forecast, not finish yet

--- WeatherApp/src/components/Card/index.tsx
+++ WeatherApp/src/components/Card/index.tsx
@@ -1,10 +1,40 @@
 
 
-const Card = () => {
+const Card: React.FC = () => {
+
+    // const apiKey = "c1bbe714ac530d052910f744c4c3da7b"
     
     return (
-        <div>
-            
+        <div className="bg-red-200">
+            <div className="flex flex-col justify-center items-center bg-blue-300">
+            <div>3 hour forecast data</div>
+            <div className="flex justify-center items-center space-x-10">
+            <div>waktu</div>
+                <div className="flex flex-col justify-center items-center">
+                    <div><img src="" alt="" />icon awan</div>
+                    <div>suhu</div>
+                </div>
+            <div>deskripsi</div>
+            </div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-10">
+            <div>waktu</div>
+                <div className="flex flex-col justify-center items-center">
+                    <div><img src="" alt="" />icon awan</div>
+                    <div>suhu</div>
+                </div>
+            <div>deskripsi</div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-10">
+            <div>waktu</div>
+                <div className="flex flex-col justify-center items-center">
+                    <div><img src="" alt="" />icon awan</div>
+                    <div>suhu</div>
+                </div>
+            <div>deskripsi</div>
+            </div>
         </div>
     )
 }

8afd25a26fba69b2a0b0ac14194202692f14cb37
Gustitreshana <gustith2105@gmail.com>
2024-01-12 23:41:58 +0700

    add design for forcast data

--- WeatherApp/src/components/Card/index.tsx
+++ WeatherApp/src/components/Card/index.tsx
@@ -1,39 +1,328 @@
-
+import cloud_icon from "../../assets/cloud.png"
 
 const Card: React.FC = () => {
 
-    // const apiKey = "c1bbe714ac530d052910f744c4c3da7b"
+    //_iconst apiKey = "c1bbe714ac530d052910f744c4c3da7b"
     
     return (
-        <div className="bg-red-200">
-            <div className="flex flex-col justify-center items-center bg-blue-300">
-            <div>3 hour forecast data</div>
+        <div className="backdrop-blur-md bg-opacity-10 text-white pb-5 w-2/3">
+            <div className="flex flex-col justify-center items-center">
+            <div className="border-b border-white p-3 text-3xl">3 hour forecast data</div>
             <div className="flex justify-center items-center space-x-10">
-            <div>waktu</div>
-                <div className="flex flex-col justify-center items-center">
-                    <div><img src="" alt="" />icon awan</div>
-                    <div>suhu</div>
+                <div className="flex justify-center items-center space-x-10 pt-5 border-b-2">
+                <div>Monday</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>00</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
                 </div>
-            <div>deskripsi</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>03</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>06</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>09</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>12</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>15</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>18</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>21</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div>Cloudy</div>
+                </div>
+            
             </div>
             </div>
 
             <div className="flex justify-center items-center space-x-10">
-            <div>waktu</div>
-                <div className="flex flex-col justify-center items-center">
-                    <div><img src="" alt="" />icon awan</div>
-                    <div>suhu</div>
+            <div className="flex justify-center items-center space-x-10 pt-5 border-b-2">
+                <div>Tuesday</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>00</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
                 </div>
-            <div>deskripsi</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>03</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>06</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>09</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>12</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>15</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>18</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>21</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div>Cloudy</div>
+                </div>
             </div>
 
             <div className="flex justify-center items-center space-x-10">
-            <div>waktu</div>
-                <div className="flex flex-col justify-center items-center">
-                    <div><img src="" alt="" />icon awan</div>
-                    <div>suhu</div>
+            <div className="flex justify-center items-center space-x-10 pt-5 border-b-2">
+                <div>Wednesday</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>00</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
                 </div>
-            <div>deskripsi</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>03</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>06</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>09</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>12</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>15</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>18</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>21</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div>Cloudy</div>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-10">
+            <div className="flex justify-center items-center space-x-10 pt-5 border-b-2">
+                <div>Thursday</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>00</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>03</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>06</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>09</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>12</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>15</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>18</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>21</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div>Cloudy</div>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-10">
+            <div className="flex justify-center items-center space-x-10 pt-5 border-b-2">
+                <div>Friday</div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>00</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>03</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>06</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>09</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>12</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>15</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>18</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                    <div>21</div>
+                    <div className="w-12 h-12"><img src={cloud_icon} alt="" /></div>
+                    <div>28°C</div>
+                    </div>
+                </div>
+            <div>Cloudy</div>
+                </div>
             </div>
         </div>
     )
